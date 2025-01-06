42868cd2bb88cb41a51c6b80b1cea03e0791af76
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-05 16:02:14 -0800

    Time: 5 ms (47.82%), Space: 12 MB (20.58%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> note;
-        unordered_map<char, int> mag;
+        //unordered_map<char, int> mag;
         for (auto letter : ransomNote) {
             note[letter]++;
         }
-        for (auto letter : magazine) {
-            mag[letter]++;
-        }
+        //for (auto letter : magazine) {
+        //    mag[letter]++;
+        //}
         for (auto x : note) {
-            if (mag[x.first] < x.second) {
+            if (count(magazine.begin(), magazine.end(), x.first) < x.second) {
                 return false;
             }
         }
