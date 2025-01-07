aa0a98353a59a09091b1ff4fe246dd7cc2a3f1e2
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-06 19:03:36 -0800

    Time: 0 ms (100%), Space: 14.7 MB (35.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> nums_map;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums_map.find(target-nums[i]) != nums_map.end()) {
-                return {nums_map[target-nums[i]], i};
+            int diff = target-nums[i];
+            if (nums_map.find(diff) != nums_map.end()) {
+                return {nums_map[diff], i};
             }
             nums_map[nums[i]] = i;
-            
         }
         return {};
 
