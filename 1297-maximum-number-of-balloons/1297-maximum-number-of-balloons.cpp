031e683d24613fd29d0158bcd5a651c66071f378
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-06 18:20:49 -0800

    Time: 0 ms (100%), Space: 8.7 MB (86.71%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -5,11 +5,6 @@ public:
         for (int i = 0; i < text.size(); i++) {
             count[text[i]-'a']++;
         }
-        count['l'-'a'] /= 2;
-        count['o'-'a'] /= 2;
-        int result = min(count['b'-'a'], count[0]);
-        result = min(result, count['l'-'a']);
-        result = min(result, count['o'-'a']);
-        return min(result, count['n'-'a']);
+        return min({count[1], count[0], count[11]/2, count[14]/2, count[13]});
     }
 };
