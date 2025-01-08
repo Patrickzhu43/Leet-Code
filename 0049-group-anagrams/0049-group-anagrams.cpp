8136772a54fed1da62190a397377aa9786f02434
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-07 17:51:59 -0800

    Time: 18 ms (57.22%), Space: 26 MB (48.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anagrams;
         vector<vector<string>> result;
-        for (int i = 0; i < strs.size(); i++) {
-            string sorted = strs[i];
+        for (auto word : strs) {
+            string sorted = word;
             sort(sorted.begin(), sorted.end());
-            anagrams[sorted].push_back(strs[i]);
+            anagrams[sorted].push_back(word);
         }
         for (auto it : anagrams) {
             result.push_back(it.second);
