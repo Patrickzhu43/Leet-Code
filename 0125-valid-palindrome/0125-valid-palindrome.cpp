65721e5bdde3bf5299b0b9fa98d17f69bc7e8604
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-08 01:49:18 -0800

    Time: 0 ms (100%), Space: 9.7 MB (68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (char &c : s) {
-            c = tolower(c);
-        }
-        int i = 0;
-        int len = s.size();
-        while (i < len) {
-            if ((s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9')) {
-                s.erase(i,1);
-                len--;
-            } 
-            else {
-                i++;
-            }
-        }
         int left = 0;
         int right = s.size()-1;
         while (left < right) {
-            if (s[left] != s[right]) {
+            if (!isalnum(s[left])) {
+                left++;
+                continue;
+            }
+            if (!isalnum(s[right])) {
+                right--;
+                continue;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             left++;
