45884252023e42c8f8e01981af1b1df8da97b3e6
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-08 01:27:41 -0800

    Time: 0 ms (100%), Space: 27.2 MB (82.07%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         int left = 0;
         int right = s.size()-1;
         while (left < right) {
-            swap(s[left], s[right]);
-            left++;
-            right--;
+            swap(s[left++], s[right--]);
         }
     }
 };
