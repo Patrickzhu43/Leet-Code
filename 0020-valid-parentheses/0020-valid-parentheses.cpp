35b3a96f5c5f56b64ef86bd73f2ceb94487e9a1e
Patrickzhu43 <55116479+Patrickzhu43@users.noreply.github.com>
2025-01-09 01:14:03 -0800

    Time: 0 ms (100%), Space: 8.9 MB (28.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> brackets;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c=='{') {
+                brackets.push(c);
+                continue;
+            }
+            if ((c == ')' || c == ']' || c == '}') && brackets.empty()) {
+                return false;
+            }
+            if ((c == ')' && brackets.top() == '(') || (c == ']' && brackets.top() == '[') || (c == '}' && brackets.top() == '{')) {
+                brackets.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return brackets.empty();
+    }
+};
